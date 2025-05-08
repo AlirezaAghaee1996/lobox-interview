b0bbe017f887cd9e9775147299da252fd82a3f13
AlirezaAghaee1996 <alireza.aghaee70@gmail.com>
2025-05-09 02:52:57 +0330

    Refactor App component to render Home page and remove unused flex mixin from styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import Home from "./Pages/Home";
 
 const App: React.FC = () => {
-  return (
-    <>
-    </>
-  );
+  return <>
+    <Home />
+  </>;
 };
-
 export default App;
