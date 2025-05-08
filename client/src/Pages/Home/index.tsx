90038548325ab67d9e87526574a07f78996daa60
AlirezaAghaee1996 <alireza.aghaee70@gmail.com>
2025-05-09 02:53:42 +0330

    Add Home component with styles and MultiDropDown integration

--- client/src/Pages/Home/index.tsx
+++ client/src/Pages/Home/index.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import MultiDropDown from '../../Components/MultiDropDown';
+import './Home.scss'
 const Home: React.FC = () => {
+  const [items, setItems] = useState<any[]>([
+    { label: 'Physics', value: 'physics', select: false },
+    { label: 'Chemistry', value: 'chemistry', select: true },
+    { label: 'Biology', value: 'biology', select: false },
+  ]);
+  
+  const handleChange = (val: any) => {
+    setItems([...items, val]);
+  };
+  const handleSelect = (selectedItems: any[]) => {
+    setItems(selectedItems);
+  };
   return (
-    <>
-    </>
+    <div className='home-container'>
+        <MultiDropDown items={items} name="Science"   handleChange={handleChange} placeholder="Science" handleSelect={handleSelect} />
+    </div>
   );
 };
 
